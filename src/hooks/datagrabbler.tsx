ba3b52bea6c80b762a6f754fb2ada80d62cf199b
SamuelV1 <Samuelvic@protonmail.com>
2021-10-26 00:27:10 -0300

    config

---
+++ src/hooks/datagrabbler.tsx
@@ -0,0 +1,7 @@
+
+import data from '../data/Questions.json'
+
+export function useData(tipo :string) {
+    
+    return data.filter(item => item.field1 !== tipo) 
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useData } from '../../hooks/datagrabbler'
+
+export default function Home() {
+const info = useData("merlin")
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
